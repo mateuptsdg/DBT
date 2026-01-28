be83659c9759bb7468fcb307373cf6d5aa811aa3
mateu.pastor@sdggroup.com <mateu.pastor@sdggroup.com>
2026-01-28 15:15:07 +0000

    created macro cents_to_dollars + documentation and implementation

---
+++ models/intermediate/int_orders__pivoted.sql
@@ -0,0 +1,6 @@
+with payments as (
+select * from {{ref('stg_stripe__payments')}}
+where status='success'),
+pivoted as (select order_id, sum(case when payment_method = 'bank_transfer' then amount else 0 end) as bank_transfer_amount 
+from payments group by 1) 
+select * from pivoted
