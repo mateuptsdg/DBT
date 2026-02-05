26f5984f49155a05ca92623fd52e66bee7ec5565
Mateu Pastor Torrens <mateu.pastor@sdggroup.com>
2026-02-05 12:08:39 +0000

    updates

--- models/marts/fact_Order.sql
+++ models/marts/fact_Order.sql
@@ -1,14 +1,26 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='lineitem_id',
+        on_schema_change='fail'
+    )
+}}
+
 with orders as (
     select * from {{ ref('stg_tpch__orders') }}
 ),
 
 line_items as (
     select * from {{ ref('stg_tpch__lineitem') }}
+    
+    {% if is_incremental() %}
+    -- Solo traemos las líneas cuya fecha sea posterior a la última cargada en esta Fact
+    where ship_date > (select max(ship_date) from {{ this }})
+    {% endif %}
 ),
 
 final as (
     select
-        -- Usamos los nombres nuevos que definiste en el Staging
         li.lineitem_id,
         li.order_id,
         li.part_id,
@@ -18,7 +30,7 @@ final as (
         o.status_code as order_status,
         li.return_flag,
         
-        -- Aplicamos las macros con los nombres nuevos
+        -- Tu macro genérica para limpiar el prefijo
         {{ remove_prefix('o.clerk_name') }} as clerk_id,
         
         {{ calculate_net_amount('li.extended_price', 'li.discount_percentage', 'li.tax_rate') }} as net_item_sales_amount,
