4c2cb5b0d2ce38d17c151bd36f0decb65a7a335e
Mateu Pastor Torrens <mateu.pastor@sdggroup.com>
2026-02-17 10:30:25 +0000

    added snsh to marts layer

--- models/marts/core/dim_Parts.sql
+++ models/marts/core/dim_Parts.sql
@@ -1,16 +1,17 @@
 with snapshot_parts as (
-    -- Llamamos directamente al snapshot que creamos antes
     select * from {{ ref('snsh_parts') }}
 ),
 
 final as (
     select
         {{ dbt_utils.generate_surrogate_key(['part_id', 'dbt_valid_from']) }} as part_key,
-        
         part_id,
         part_name,
-        manufacturer_name,
-        brand_name,
+        {{ remove_prefix('manufacturer_name') }} as manufacturer_num,
+        {{ remove_prefix('brand_name') }} as brand_num,
+        part_type,
+        part_size,
+        container_type,
         retail_price,
         dbt_valid_from as valid_from,
         dbt_valid_to as valid_to,
@@ -24,3 +25,5 @@ final as (
 )
 
 select * from final
+
+        
